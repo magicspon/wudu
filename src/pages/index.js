017e7db01ba14874682cd7567a74eb5ba5b0d2d5
magicspon <magicspon@gmail.com>
2020-07-24 13:52:53 +0100

    added some example code

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,20 @@ import * as React from "react"
 // import * as T from "prop-types"
 import gqlClient from "@/utils/gqlClient"
 import pageQuery from "@/queries/home"
+import GraphImg from "graphcms-image"
 
 export default function IndexPage({ data }) {
+	const { images } = data.homePage
+
+	console.log({ data })
+
 	return (
 		<div>
-			<pre>{JSON.stringify(data, null, 2)}</pre>
+			<div>
+				{images.map((image) => (
+					<GraphImg key={image.id} image={image} maxWidth={800} />
+				))}
+			</div>
 		</div>
 	)
 }
